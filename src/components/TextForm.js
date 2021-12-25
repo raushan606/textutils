e25271a4d71eee1c5701c5811f1cacc7b787d6a0
raushan606 <raushankumar606@gmail.com>
2021-12-25 22:59:46 +0530

    video#9

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard/lib/Component";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
@@ -18,6 +19,31 @@ export default function TextForm(props) {
   const handleClearBox = () => {
     setText("");
   };
+
+  const handleCopyClipboard = () => {
+    alert("Text Copied to Clipboard");
+  };
+
+  const handleDownloadText = () => {
+    const element = document.createElement("a");
+    const file = new Blob([document.getElementById("myBox").value], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "myFile.txt";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  const handleAlternateCase = () => {
+    setText(
+      text
+        .split("")
+        .map((c, i) => (i % 2 === 0 ? c.toLowerCase() : c.toUpperCase()))
+        .join("")
+    );
+  };
+
   const [text, setText] = useState("Enter Text Here");
   return (
     <>
@@ -28,25 +54,53 @@ export default function TextForm(props) {
             className="form-control"
             value={text}
             onChange={handleOnChange}
-            onClick={handleClearBox}
             id="myBox"
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary btn-sm mx-2" onClick={handleUpClick}>
+        <button
+          className="btn btn-primary btn-sm mx-2 my-2"
+          onClick={handleUpClick}
+        >
           Convert to UpperCase
         </button>
         <button
-          className="btn btn-primary btn-sm mx-2"
+          className="btn btn-primary btn-sm mx-2 my-2"
           onClick={handleLowClick}
         >
           Convert to LowerCase
         </button>
+        <button
+          className="btn-outline btn-primary btn-sm mx-2 my-2"
+          onClick={handleClearBox}
+        >
+          Clear Text
+        </button>
+        <CopyToClipboard text={text}>
+          <button
+            className="btn btn-primary btn-sm mx-2 my-2"
+            onClick={handleCopyClipboard}
+          >
+            Copy To Clipboard
+          </button>
+        </CopyToClipboard>
+        <button
+          className="btn btn-primary btn-sm mx-2 my-2"
+          onClick={handleDownloadText}
+        >
+          Download Text
+        </button>
+        <button
+          className="btn btn-primary btn-sm mx-2 my-2"
+          onClick={handleAlternateCase}
+        >
+          aLtErNaTe TeXt
+        </button>
       </div>
-      <div className="container">
+      <div className="container my-2 border">
         <h1>Your Text Summary</h1>
         <p>
-          {text.split(" ").length} words {text.length} characters
+          {text.split(" ").length} words | {text.length} characters
         </p>
         <p>{(0.08 * text.length).toFixed(1)} Minutes read.</p>
         <h2>Preview</h2>
