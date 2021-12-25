bcca37f0b3021018fda71b04280e6d303402a0fe
raushan606 <raushankumar606@gmail.com>
2021-12-25 22:35:29 +0530

    video #8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,36 +2,56 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("Button was clicked");
     let newText = text.toUpperCase();
     setText(newText);
   };
 
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
-    console.log("handle on change");
     setText(event.target.value);
   };
 
   const handleClearBox = () => {
     setText("");
-  }
+  };
   const [text, setText] = useState("Enter Text Here");
   return (
-    <div>
-      <div>{props.heading}</div>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          onClick={handleClearBox}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            onClick={handleClearBox}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary btn-sm mx-2" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button
+          className="btn btn-primary btn-sm mx-2"
+          onClick={handleLowClick}
+        >
+          Convert to LowerCase
+        </button>
       </div>
-      <button className="btn btn-primary btn-sm" onClick={handleUpClick}>
-        Convert to UpperCase
-      </button>
-    </div>
+      <div className="container">
+        <h1>Your Text Summary</h1>
+        <p>
+          {text.split(" ").length} words {text.length} characters
+        </p>
+        <p>{(0.08 * text.length).toFixed(1)} Minutes read.</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
